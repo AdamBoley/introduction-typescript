897ca0bbc460a3c36b8ada5d507b8bdac69a9118
AdamBoley <adamboley92@gmail.com>
2021-08-29 16:17:36 +0000

    Loops

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 //console.log("Hello, World");
 
-for (let i = 0; i < 3; i++) {
+for (let i = 2; i < 7; i++) {
     console.log ("Block statement execution no." + i);
   }
+
+  // for (let i = firstNumber; i < (secondNumber - 1); i++);
